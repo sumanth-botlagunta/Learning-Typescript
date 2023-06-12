111238756c003189d6f3964fc3a8fcb3b0a065fa
sumanth-botlagunta <sumanthbotlagunta977@gmail.com>
2023-06-12 10:56:00 +0530

    basics of typescript

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,5 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 console.log('Hello TypeScript!');
 let age = 50;
 if (age > 20) {
@@ -17,4 +18,33 @@ for (const i in numbers) {
     strings.push(i.toString());
 }
 console.log(strings);
+let user = [1, 'Sumanth'];
+let mySize = 3;
+console.log(mySize);
+function caluclateTax(income, taxYear = 2023) {
+    if (income < 50000 && taxYear < 2022) {
+        return income * 1.2;
+    }
+    else if (income < 60000) {
+        return income * 1.25;
+    }
+    return income * 1.3;
+}
+let myTax = caluclateTax(90000, 2021);
+let employee = {
+    id: 1,
+    name: 'Sumanth',
+    retire: (date) => {
+        console.log(date);
+    },
+};
+function kgstolbs(weight) {
+    if (typeof weight === 'number')
+        return weight * 2.2;
+    else
+        return parseInt(weight) * 2.2;
+}
+console.log(kgstolbs(10), kgstolbs('10kgs'));
+let myquanity = 100;
+console.log(`the quanity is ${myquanity}`);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import { type } from 'os';
+
 console.log('Hello TypeScript!');
 let age: number = 50;
 if (age > 20) {
@@ -21,3 +23,57 @@ for (const i in numbers) {
 }
 
 console.log(strings);
+
+// tuples
+let user: [number, string] = [1, 'Sumanth'];
+
+// enums
+const enum Sizes {
+  small = 1,
+  medium,
+  large,
+}
+let mySize: Sizes = Sizes.large;
+console.log(mySize);
+
+// functions
+function caluclateTax(income: number, taxYear: number = 2023): number {
+  if (income < 50_000 && taxYear < 2022) {
+    return income * 1.2;
+  } else if (income < 60_000) {
+    return income * 1.25;
+  }
+  return income * 1.3;
+}
+
+let myTax: number = caluclateTax(90_000, 2021);
+
+// objects
+// type alliases
+type Employee = {
+  readonly id: number;
+  name: string;
+  retire: (date: Date) => void;
+};
+
+let employee: Employee = {
+  id: 1,
+  name: 'Sumanth',
+  retire: (date: Date) => {
+    console.log(date);
+  },
+};
+
+// union types
+function kgstolbs(weight: number | string): number {
+  if (typeof weight === 'number') return weight * 2.2;
+  else return parseInt(weight) * 2.2;
+}
+console.log(kgstolbs(10), kgstolbs('10kgs'));
+
+// literals
+type Quantity = 50 | 100 | 150;
+let myquanity: Quantity = 100;
+console.log(`the quanity is ${myquanity}`);
+
+// optional chaining
